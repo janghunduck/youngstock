c91a99901712b74be5e70fddd565e106479a3c17
janghunduck <60320595+janghunduck@users.noreply.github.com>
2026-03-02 13:41:36 +0900

    Update youngstock.js

--- youngstock.js
+++ youngstock.js
@@ -95,7 +95,9 @@ async function getFscData(authkey, code, displayloc){
 
 
 async function getServiceKey(){
+    alert('0');
     const url= 'https://blog.kakaocdn.net/dna/B8LTS/dJMcai3rLsg/AAAAAAAAAAAAAAAAAAAAAOLVmrn1Dl5_fV9yZ1N48fXQRUCeCOAJwjj3kdqVeUTX/fsc.key?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1774969199&allow_ip=&allow_referer=&signature=N8geB9ZrqVKaBbobGlFiRkpS8vU%3D&attach=1&knm=tfile.key';
+    alert('1');
     const response = await fetch(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -106,6 +108,7 @@ async function getServiceKey(){
     const htmlString = await response.text();
     alert(htmlString);
 }
+
 
 async function crawl(url) {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';   //cors 우회 프록시 서버 URL
@@ -269,6 +272,7 @@ async function getCoinPrice(code, displayloc) {
         };
 
 }
+
 
 
 
