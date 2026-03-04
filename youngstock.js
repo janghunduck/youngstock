1370e71bfb9184ac55dfcf7146c94a3b4cad018d
janghunduck <60320595+janghunduck@users.noreply.github.com>
2026-03-05 00:29:06 +0900

    Update youngstock.js

--- youngstock.js
+++ youngstock.js
@@ -216,8 +216,8 @@ async function getServiceKey(){
     const url= 'https://youngsto.tistory.com/58';
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; //cors 우회 프록시 서버 URL
     const decodedUrl = decodeURI( url );
-    log += `<p>${proxyUrl} + ${url}</br></p>`;
-    log += `<p>${proxyUrl} + ${decodedUrl}</br></p>`;
+    log += `<p>${proxyUrl}${url}</br></p>`;
+    log += `<p>${proxyUrl}${decodedUrl}</br></p>`;
     const response = await fetch(proxyUrl + url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -403,6 +403,7 @@ async function getCoinPrice(code, displayloc) {
         };
 
 }
+
 
 
 
