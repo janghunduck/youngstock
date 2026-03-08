a78aa2d5f69f09dd3efbbe3d4cf5aca407190df0
janghunduck <60320595+janghunduck@users.noreply.github.com>
2026-03-08 10:03:52 +0900

    Update youngstock.js

--- youngstock.js
+++ youngstock.js
@@ -189,6 +189,60 @@ async function crawlcd(code, displayloc) {
     }
 }
 
+async function crawlcd(code, displayloc) {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; //cors 우회 프록시 서버 URL
+    const url= 'https://finance.naver.com/item/main.naver?code=' + code;
+    const decodedUrl = decodeURI( url );
+    const response = await fetch(proxyUrl + url, {
+        // method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
+        }
+    });
+    if (!response.ok) {   // 실패 
+        // 여기서 부터는 재귀를 사용하고, 파싱에서 뽑을 때는 url를 체크(도메인)해서 처리한다.
+        // crawlOther(code, displayloc, url);
+        // finup.co.kr에 접속해 가져온다. 우회경로 사용.
+        // response.status http응답코드, response.headers 
+        // throw new Error(`HTTP error! status: ${response.status}`);  // If not ok (e.g., 404, 500), throw an error to be caught by the catch block
+    } else {  // 성공,  200~299이면 true
+        const htmlString = await response.text();
+        const parser = new DOMParser();
+        const htmlDOM = parser.parseFromString(htmlString, 'text/html');
+        const items = htmlDOM.querySelectorAll('.blind');
+        let  itemslen = items.length;
+        var totaltxt = '';
+        var result = '';
+
+        for (let i = 0; i < itemslen; i++) {
+           totaltxt = totaltxt + items[i].textContent;
+           if (items[i].textContent.includes('현재')) {
+               var tagdls = items[i].getElementsByTagName('dd');  // HTMLcollector object  vi getElementsByClassName
+
+               for (var j = 0; j < 4; j++) {
+                  var ticker = tagdls[j].textContent + '   ';  //getElementById
+                  ticker = ticker.replace(/\n/g, ' ');
+                  if(tagdls[j].textContent .includes('종목')){ } else {
+                      result = result + '\n' + ticker;
+                  }
+               }
+           }
+        }
+
+        result = result.replaceAll('현재가','');
+        result = result.replaceAll('전일대비',' ');
+        result = result.replaceAll('하락','▼');
+        result = result.replaceAll('상승','▲');
+        result = result.replaceAll('보합',', ');
+        result = result.replaceAll('마이너스',', -');
+        result = result.replaceAll('플러스',', +');
+        result = result.replaceAll('퍼센트','%');
+    
+        document.getElementById(displayloc).innerText = `${result}`; 
+    }
+}
+
 /*
 티스토리에서 코드를 줄이기 위해 여기서 구성한다.
 <div id="console_result">&nbsp;</div>
@@ -213,6 +267,7 @@ function _gethostname(urlString){
 crawlcd 에서 응답이 없는 경우 crawlcd 함수 안에서 호출된다.
 crawlcd 함수와 동일하나 urlSting(https://....)를 인자로 받는다.
 이 함수는 재귀함수로 다시 응답이 없으면 다른 url로 재 호출하고 url의 hostname를 구분자로 파싱을 수행한다. 
+cors 정책이 느슨한 사이트는 어디인가?
 ------------------------------------------------------------------------------------------------ */
 async function crawlOther(code, displayloc, urlString) {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; //cors 우회 프록시 서버 URL
@@ -238,6 +293,7 @@ async function crawlOther(code, displayloc, urlString) {
         var totaltxt = '';
         var result = '';
 
+        // url 분기 처리
         for (let i = 0; i < itemslen; i++) {
            totaltxt = totaltxt + items[i].textContent;
            if (items[i].textContent.includes('현재')) {
@@ -252,7 +308,7 @@ async function crawlOther(code, displayloc, urlString) {
                }
            }
         }
-
+       
         result = result.replaceAll('현재가','');
         result = result.replaceAll('전일대비',' ');
         result = result.replaceAll('하락','▼');
@@ -473,12 +529,19 @@ async function crawlc(code) {
     //document.getElementById('console-output').innerText = `${totaltxt}`; 
 }
 
-
-
-async function crawlhtml(url) {
+/*
+  isCors : cors 정책으로 porxy를 탈건지 말건지 결정, true or false
+*/
+async function crawlhtml(url, isCors) {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';   //cors 우회 프록시 서버 URL
     const decodedUrl = decodeURI(url);
-    const response = await fetch(proxyUrl + url, {
+    var joinurl ='';
+    if (isCors){
+        joinurl = proxyUrl + url; 
+    } else {
+        joinurl = url; 
+    }
+    const response = await fetch(joinurl, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
@@ -606,6 +669,7 @@ kg모빌리티 1000
 2026년 03월 06일 16시 10분 기준 장마감
 3,505 ▼ 5 , - 0.14 %
 */
+
 
 
 
